37ec52e8a4574bb3e6b2d2ae7d2bee9b4b64f6e5
akashin <kashin.andrej@gmail.com>
2018-08-11 19:40:01 +0100

    Move memory shape on conveyor to a separate file

---
+++ allocator/src/objects/memoryShapeOnConveyor.ts
@@ -0,0 +1,36 @@
+import { CONST, CONVEYOR_CONST } from "../const/const";
+import { MemoryShape } from "./memoryShape";
+import { GridCell } from "./gridCell";
+
+class MemoryShapeOnConveyor extends Phaser.GameObjects.Container {
+  private memoryShape: MemoryShape;
+
+  constructor(scene, shape, params) {
+    super(scene, params.x, params.y);
+
+    this.memoryShape = shape;
+    this.setInteractive();
+
+    for (var w = 0; w < this.memoryShape.getWidth(); ++w) {
+      for (var h = 0; h < this.memoryShape.getHeight(); ++h) {
+        if (this.memoryShape.hasCell(w, h)) {
+          // TODO: Use a separate grid cell type.
+          let cell = new GridCell(scene, {
+            x: w * CONVEYOR_CONST.SHAPE_CELL_WIDTH,
+            y: h * CONVEYOR_CONST.SHAPE_CELL_HEIGHT,
+            width: CONVEYOR_CONST.SHAPE_CELL_WIDTH,
+            height: CONVEYOR_CONST.SHAPE_CELL_HEIGHT,
+          });
+          this.add(cell);
+        }
+      }
+    }
+
+    scene.add.existing(this);
+  }
+
+  setChosen() {
+    console.log("Shape on conveyor clicked");
+    this.setAlpha(0.5);
+  }
+}
--- allocator/src/objects/shapeConveyor.ts
+++ allocator/src/objects/shapeConveyor.ts
@@ -1,6 +1,6 @@
 import { CONST, CONVEYOR_CONST } from "../const/const";
 import { MemoryShape } from "./memoryShape";
-import { GridCell } from "./gridCell";
+import { MemoryShapeOnConveyor } from "./memoryShapeOnConveyor";
 import { TiledLayout, LayoutDirection } from "../utils/layout";
 
 export class ShapeConveyor extends Phaser.GameObjects.Container {
@@ -28,32 +28,5 @@ export class ShapeConveyor extends Phaser.GameObjects.Container {
       layout.addItem(shape);
     }
     this.add(layout);
-  }
-}
-
-class MemoryShapeOnConveyor extends Phaser.GameObjects.Container {
-  private memoryShape: MemoryShape;
-
-  constructor(scene, shape, params) {
-    super(scene, params.x, params.y);
-
-    this.memoryShape = shape;
-
-    for (var w = 0; w < this.memoryShape.getWidth(); ++w) {
-      for (var h = 0; h < this.memoryShape.getHeight(); ++h) {
-        if (this.memoryShape.hasCell(w, h)) {
-          // TODO: Use a separate grid cell type.
-          let cell = new GridCell(scene, {
-            x: w * CONVEYOR_CONST.SHAPE_CELL_WIDTH,
-            y: h * CONVEYOR_CONST.SHAPE_CELL_HEIGHT,
-            width: CONVEYOR_CONST.SHAPE_CELL_WIDTH,
-            height: CONVEYOR_CONST.SHAPE_CELL_HEIGHT,
-          });
-          this.add(cell);
-        }
-      }
-    }
-
-    scene.add.existing(this);
   }
 }
